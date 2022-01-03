164859ec3d637cb59201482ca20f31e0fb085e9f
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2022-01-03 16:31:22 -0700

    communication with lambda function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "capstoneproject96034c9d": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "capstoneproject": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
