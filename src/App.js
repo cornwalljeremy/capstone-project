6a36655d89e08b663535fa2692405139cafc1c02
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2022-01-03 18:14:34 -0700

    Working demo published to AWS usin Amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Amplify, { API } from "aws-amplify";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const myAPI = "capstoneproject";
+const path = "/user";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [user, setUser] = useState([]);
+
+  function getUser(e) {
+    let userId = e.input;
+    API.get(myAPI, path + "/" + userId)
+      .then((response) => {
+        console.log(response);
+        let newUser = [...user];
+        newUser.push(response);
+        setUser(newUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Super Simple React App</h1>
+      <div>
+        <input
+          placeholder='user id'
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <br />
+      <button onClick={() => getUser({ input })}>
+        Get User From Backend
+      </button>
+
+      <h2 style={{ visibility: user.length > 0 ? "visible" : "hidden" }}>
+        Response
+      </h2>
+      {user.map((thisUser, index) => {
+        return (
+          <div key={thisUser.userId}>
+            <span>
+              <b>userId:</b> {thisUser.userId} - <b>userName</b>
+              : {thisUser.userName}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
